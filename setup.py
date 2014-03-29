a936062639be3f8ec3ae9582059195ad6d8cf327
James Muscat <jamesremuscat@gmail.com>
2014-03-29 22:16:20 +0000

    Set up some setup.py things to make testing work.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='av-control',
+    version='0.92',
+    description='User interface for controlling the A/V devices at St Aldates church',
+    author='James Muscat',
+    author_email='jamesremuscat@gmail.com',
+    url='https://github.com/staldates/av-control',
+    install_requires=["avx>=0.92"],
+    dependency_links = [
+        "lib/" # find Affle packages here
+        ],
+    packages=find_packages('src', exclude=["*.tests"]),
+    package_dir = {'':'src'},
+      )
