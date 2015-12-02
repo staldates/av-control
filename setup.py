6ebfe27e6c67657872f9261a8297e37d20e08195
James Muscat <jamesremuscat@gmail.com>
2015-12-02 15:38:12 +0000

    Update version numbers.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='av-control',
-    version='0.93',
+    version='0.95',
     description='User interface for controlling the A/V devices at St Aldates church',
     author='James Muscat',
     author_email='jamesremuscat@gmail.com',
     url='https://github.com/staldates/av-control',
-    install_requires=['avx>=0.93', 'mock', 'nose', 'nosexcover'],
+    install_requires=['avx>=0.95', 'mock', 'nose', 'nosexcover'],
     dependency_links = [
         "lib/" # find local copies of packages here
         ],
