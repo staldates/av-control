ef96a46bef4d45be9523e5c6573bb27372055df6
James Muscat <jamesremuscat@gmail.com>
2016-01-02 13:44:41 +0000

    Bump version number (and add explanatory comment).

--- src/staldates/ui/_version.py
+++ src/staldates/ui/_version.py
@@ -1,1 +1,4 @@
-__version__ = "0.95"
+# Note: this is not the same as the avx library version!
+# It is the latter that gets checked for compatibility with the controller.
+# not this version string. But it keeps things simpler to keep them in step.
+__version__ = "0.96.0"
