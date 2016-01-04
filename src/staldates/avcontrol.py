a66527e66aa7cb50995c6cb121c7f29e84d769fc
James Muscat <jamesremuscat@gmail.com>
2016-01-04 14:03:35 +0000

    Reimplement PySide-specific client methods.
    These have just been ripped out of avx due to the dependency on PySide.

--- src/staldates/avcontrol.py
+++ src/staldates/avcontrol.py
@@ -6,9 +6,10 @@ import sys
 
 from avx.Client import Client
 from avx.controller.Controller import Controller, VersionMismatchError
+from avx.StringConstants import StringConstants
 from pkg_resources import require
 from Pyro4.errors import NamingError, CommunicationError
-from PySide.QtCore import Qt, QFile
+from PySide.QtCore import Qt, QFile, QObject, QCoreApplication, QEvent
 from PySide.QtGui import QApplication
 from staldates.ui import resources  # @UnusedImport  # Initialises the Qt resources
 from staldates.ui.MainWindow import MainWindow
@@ -16,6 +17,58 @@ from staldates.ui.widgets import Dialogs
 
 
 require("avx>=0.95")
+
+
+class AvControlClient(Client):
+
+    def __init__(self, avcontrol):
+        super(AvControlClient, self).__init__()
+        self.avcontrol = avcontrol
+
+    def errorBox(self, text):
+        invoke_in_main_thread(self.avcontrol.errorBox, text)
+        return True
+
+    def showPowerOnDialog(self):
+        invoke_in_main_thread(self.avcontrol.showPowerDialog, StringConstants.poweringOn)
+        return True
+
+    def showPowerOffDialog(self):
+        invoke_in_main_thread(self.avcontrol.showPowerDialog, StringConstants.poweringOff)
+        return True
+
+    def hidePowerDialog(self):
+        invoke_in_main_thread(self.avcontrol.hidePowerDialog)
+        return True
+
+    def updateOutputMappings(self, mapping):
+        invoke_in_main_thread(self.aldatesx.updateOutputMappings, mapping)
+        return True
+
+
+class InvokeEvent(QEvent):
+    EVENT_TYPE = QEvent.Type(QEvent.registerEventType())
+
+    def __init__(self, fn, *args, **kwargs):
+        QEvent.__init__(self, InvokeEvent.EVENT_TYPE)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+
+class Invoker(QObject):
+
+    def event(self, event):
+        event.fn(*event.args, **event.kwargs)
+
+        return True
+
+_invoker = Invoker()
+
+
+def invoke_in_main_thread(fn, *args, **kwargs):
+    QCoreApplication.postEvent(_invoker,
+                               InvokeEvent(fn, *args, **kwargs))
 
 
 def main():
@@ -56,7 +109,7 @@ def main():
 
         myapp = MainWindow(controller)
 
-        client = Client(myapp)
+        client = AvControlClient(myapp)
         client.setDaemon(True)
         client.start()
         client.started.wait()
