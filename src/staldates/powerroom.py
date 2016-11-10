2f5f8022d2255a5800ed7b295e3aee40f8eb9d13
James Muscat <jamesremuscat@gmail.com>
2016-11-10 13:27:17 +0000

    Start creating a power room UI based on existing UI.

---
+++ src/staldates/powerroom.py
@@ -0,0 +1,87 @@
+from PySide.QtCore import Qt, QFile, QCoreApplication
+from PySide.QtGui import QApplication
+from avx.controller.Controller import Controller, VersionMismatchError
+import argparse
+import atexit
+import fcntl  # @UnresolvedImport
+import logging
+import sys
+
+import Pyro4
+
+from avcontrol import AvControlClient, InvokeEvent, Invoker
+from staldates.ui import resources  # @UnusedImport  # Initialises the Qt resources
+from staldates.ui.PowerRoom import PowerRoomControl
+from staldates.ui.widgets import Dialogs
+from staldates.ui.widgets.Dialogs import handlePyroErrors
+
+
+Pyro4.config.COMMTIMEOUT = 3  # seconds
+
+_invoker = Invoker()
+
+
+def invoke_in_main_thread(fn, *args, **kwargs):
+    QCoreApplication.postEvent(_invoker,
+                               InvokeEvent(fn, *args, **kwargs))
+
+
+@handlePyroErrors(extraMessage="Cannot start application due to a network error -")
+def main():
+    pid_file = 'av-control.pid'
+    fp = open(pid_file, 'w')
+    try:
+        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        # another instance is running
+        print "av-control is already running."
+        sys.exit(1)
+
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                        level=logging.INFO)
+    app = QApplication(sys.argv)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fullscreen",
+                        help="Run in fullscreen mode and hide the mouse cursor",
+                        action="store_true")
+    parser.add_argument("-c",
+                        help="Specify the controller ID to connect to",
+                        metavar="CONTROLLERID",
+                        default="")
+    args = parser.parse_args()
+
+    try:
+        ssf = QFile(":/stylesheet")
+        ssf.open(QFile.ReadOnly)
+        styleSheet = str(ssf.readAll())
+        app.setStyleSheet(styleSheet)
+    except IOError:
+        # never mind
+        logging.warn("Cannot find stylesheet, using default system styles.")
+
+    try:
+        controller = Controller.fromPyro(args.c)
+
+        myapp = PowerRoomControl(controller)
+
+        client = AvControlClient(myapp)
+        client.setDaemon(True)
+        client.start()
+        client.started.wait()
+        atexit.register(lambda: controller.unregisterClient(client.uri))
+
+        controller.registerClient(client.uri)
+
+        if args.fullscreen:
+            QApplication.setOverrideCursor(Qt.BlankCursor)
+            myapp.showFullScreen()
+        else:
+            myapp.show()
+        sys.exit(app.exec_())
+
+    except VersionMismatchError as e:
+        Dialogs.errorBox(str(e))
+
+if __name__ == '__main__':
+    main()
---
+++ src/staldates/ui/PowerRoom.py
@@ -0,0 +1,132 @@
+from PySide.QtGui import QGridLayout, QIcon, QMainWindow, QStackedWidget, QWidget,\
+    QHBoxLayout
+from PySide.QtCore import Qt
+from staldates.ui.widgets.Buttons import ExpandingButton
+from staldates.ui.widgets.Clock import Clock
+from staldates.ui.widgets.SystemPowerWidget import SystemPowerWidget
+from staldates.ui.VideoSwitcher import VideoSwitcher
+from staldates.ui.widgets.Dialogs import PowerNotificationDialog
+from staldates.ui.widgets.BlindsControl import BlindsControl
+from staldates.ui.widgets.ProjectorScreensControl import ProjectorScreensControl
+from staldates.ui.widgets.AdvancedMenu import AdvancedMenu
+from staldates.ui.widgets import Dialogs
+from staldates.ui.widgets.LightingControl import LightingControl
+from staldates.ui.widgets.Status import SystemStatus
+
+
+class PowerRoomControl(QMainWindow):
+
+    def __init__(self, controller):
+        super(PowerRoomControl, self).__init__()
+        self.controller = controller
+
+        self.setWindowTitle("av-control - power room interface")
+        self.resize(1024, 600)
+        self.setWindowIcon(QIcon(":icons/video-display"))
+
+        self.mainScreen = VideoSwitcher(controller, self)
+        self.stack = QStackedWidget()
+        self.stack.addWidget(self.mainScreen)
+
+        outer = QWidget()
+        mainLayout = QGridLayout()
+        mainLayout.addWidget(self.stack, 0, 0, 1, 7)
+
+        column = 0
+
+        self.spc = SystemPowerWidget(controller, self)
+
+        syspower = ExpandingButton()
+        syspower.setText("Power")
+        syspower.clicked.connect(self.showSystemPower)
+        syspower.setIcon(QIcon(":icons/system-shutdown"))
+        syspower.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        mainLayout.addWidget(syspower, 1, column)
+        column += 1
+
+        self.bc = BlindsControl(controller["Blinds"], self)
+
+        blinds = ExpandingButton()
+        blinds.setText("Blinds")
+        blinds.clicked.connect(lambda: self.showScreen(self.bc))
+        blinds.setIcon(QIcon(":icons/blinds"))
+        blinds.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        mainLayout.addWidget(blinds, 1, column)
+        column += 1
+
+        self.sc = ProjectorScreensControl(controller["Screens"], self)
+
+        screens = ExpandingButton()
+        screens.setText("Screens")
+        screens.clicked.connect(lambda: self.showScreen(self.sc))
+        screens.setIcon(QIcon(":icons/screens"))
+        screens.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        mainLayout.addWidget(screens, 1, column)
+        column += 1
+
+        if controller.hasDevice("Lights"):
+            self.lightsMenu = LightingControl(controller["Lights"], self)
+
+            lights = ExpandingButton()
+            lights.setText("Lights")
+            lights.clicked.connect(lambda: self.showScreen(self.lightsMenu))
+            lights.setIcon(QIcon(":icons/lightbulb_on"))
+            lights.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+            mainLayout.addWidget(lights, 1, column)
+            column += 1
+
+        self.advMenu = AdvancedMenu(self.controller, self)
+
+        adv = ExpandingButton()
+        adv.setText("Advanced")
+        adv.setIcon(QIcon(":icons/applications-system"))
+        adv.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
+        adv.clicked.connect(lambda: self.showScreen(self.advMenu))
+        mainLayout.addWidget(adv, 1, column)
+        column += 1
+
+        for i in range(column):
+            mainLayout.setColumnStretch(i, 1)
+
+        tray = QHBoxLayout()
+        tray.addWidget(Clock())
+        tray.addWidget(SystemStatus(controller))
+        mainLayout.addLayout(tray, 1, column)
+
+        mainLayout.setRowStretch(0, 8)
+        mainLayout.setRowStretch(1, 0)
+
+        outer.setLayout(mainLayout)
+
+        self.setCentralWidget(outer)
+
+        self.pnd = PowerNotificationDialog(self)
+        self.pnd.accepted.connect(self.hidePowerDialog)
+
+    def showScreen(self, screenWidget):
+        if self.stack.currentWidget() == screenWidget:
+            self.stepBack()
+        else:
+            self.stack.insertWidget(0, screenWidget)
+            self.stack.setCurrentWidget(screenWidget)
+
+    def showSystemPower(self):
+        self.showScreen(self.spc)
+
+    def stepBack(self):
+        self.stack.removeWidget(self.stack.currentWidget())
+
+    def errorBox(self, text):
+        Dialogs.errorBox(text)
+
+    def showPowerDialog(self, message):
+        self.pnd.message = message
+        self.pnd.exec_()
+
+    def hidePowerDialog(self):
+        self.pnd.close()
+        if self.stack.currentWidget() == self.spc:
+            self.stepBack()
+
+    def updateOutputMappings(self, mapping):
+        self.mainScreen.updateOutputMappings(mapping)
