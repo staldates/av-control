c6b3076ca7ff6688e09bd6f823b455b40a673a43
James Muscat <jamesremuscat@gmail.com>
2016-11-25 17:15:10 +0000

    Break up arguments for sudo systemctl.

--- src/staldates/admin.py
+++ src/staldates/admin.py
@@ -3,7 +3,7 @@ import subprocess
 
 def restart_avx_controller():
     # NB This assumes a suitable entry in /etc/sudoers.d/ to allow the command to run passwordlessly
-    return subprocess.call(["/usr/bin/sudo /bin/systemctl restart avx-controller.service"])
+    return subprocess.call(["/usr/bin/sudo", "/bin/systemctl", "restart", "avx-controller.service"])
 
 
 def restart_server():
