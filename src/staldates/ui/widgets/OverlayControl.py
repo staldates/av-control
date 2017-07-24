00f2a959c125ef1d5a1dce7825837ff2b72d1b90
James Muscat <jamesremuscat@gmail.com>
2017-07-24 21:53:10 +0100

    Some UI polish for Visuals PC keyer screen.

--- src/staldates/ui/widgets/OverlayControl.py
+++ src/staldates/ui/widgets/OverlayControl.py
@@ -14,7 +14,9 @@ class OverlayControl(QWidget):
 
         layout = QGridLayout()
 
-        layout.addWidget(QLabel("Overlay on main output:"), 0, 0, 1, 2)
+        lbl = QLabel("Overlay on main output:")
+        lbl.setAlignment(Qt.AlignHCenter)
+        layout.addWidget(lbl, 0, 0, 1, 2)
 
         self.autoButton = ExpandingButton()
         self.autoButton.setText("Auto Fade")
@@ -27,9 +29,11 @@ class OverlayControl(QWidget):
         self.onAirButton.setCheckable(True)
         self.onAirButton.clicked.connect(self.setOnAir)
 
-        layout.addWidget(self.onAirButton, 1, 1)
+        layout.addWidget(self.onAirButton, 1, 1, 3, 1)
 
-        layout.addWidget(QLabel("Rate:"), 2, 0)
+        lblRate = QLabel("Rate:")
+        lblRate.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
+        layout.addWidget(lblRate, 2, 0)
 
         self.rate = QSpinBox()
         self.rate.setMinimum(1)
@@ -38,12 +42,20 @@ class OverlayControl(QWidget):
         self.rate.setAlignment(Qt.AlignHCenter)
         layout.addWidget(self.rate, 3, 0)
 
+        layout.setRowStretch(0, 0)
+        layout.setRowStretch(1, 1)
+        layout.setRowStretch(2, 1)
+        layout.setRowStretch(3, 1)
+
         self.update_from_dsk()
 
         self.setLayout(layout)
 
     def update_from_dsk(self):
         self.onAirButton.setChecked(self.dsk.onAir)
+        self.onAirButton.setProperty("isLive", self.dsk.onAir)
+        self.onAirButton.style().unpolish(self.onAirButton)
+        self.onAirButton.style().polish(self.onAirButton)
         self.rate.setValue(self.dsk.rate)
 
     def setOnAir(self):
