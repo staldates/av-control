94ad7c4b420d7376854ab240a343ddf1a11aa00b
James Muscat <jamesremuscat@gmail.com>
2020-07-22 15:00:10 +0100

    Lint.

--- src/staldates/ui/widgets/OverlayControl.py
+++ src/staldates/ui/widgets/OverlayControl.py
@@ -76,6 +76,6 @@ class OverlayControl(QWidget):
 
     @with_atem
     def resetParams(self):
-            self.atem.setDSKFillSource(self.dsk.idx, VideoSource.INPUT_5)
-            self.atem.setDSKKeySource(self.dsk.idx, VideoSource.INPUT_5)
-            self.atem.setDSKParams(self.dsk.idx, preMultiplied=False, gain=500, clip=210)
+        self.atem.setDSKFillSource(self.dsk.idx, VideoSource.INPUT_5)
+        self.atem.setDSKKeySource(self.dsk.idx, VideoSource.INPUT_5)
+        self.atem.setDSKParams(self.dsk.idx, preMultiplied=False, gain=500, clip=210)
